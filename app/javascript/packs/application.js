774c4f2ee96bc848d091e38a006333d9e5d86a02
Jonathan Lawerh <lawerhjonathan@gmail.com>
2019-11-25 12:42:56 +0000

    Installed Semantic UI for Frontend

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
+require ("semantic-ui")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -15,3 +17,10 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+//= require rails-ujs
+//= require jquery
+//= require activestorage
+//= require turbolinks
+//= require semantic-ui
+//= require_tree .
