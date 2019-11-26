49c67603b286abdb4f295dfae76293f3d1f1809a
Jonathan Lawerh <lawerhjonathan@gmail.com>
2019-11-26 08:31:39 +0000

    Added Favicon

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,14 +3,6 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-require("jquery")
-require ("semantic-ui")
-
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -24,3 +16,14 @@ require ("semantic-ui")
 //= require turbolinks
 //= require semantic-ui
 //= require_tree .
+
+$(document).on('turbolinks:load', function() {
+  $('.ui.dropdown').dropdown();
+  $('.message .close')
+  .on('click', function() {
+    $(this)
+      .closest('.message')
+      .transition('fade')
+    ;
+  });
+})
